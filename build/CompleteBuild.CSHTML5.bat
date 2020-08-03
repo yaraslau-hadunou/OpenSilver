2b3ab61d1ab1f314ce6703f39ccad0f9b03df10f
VSteeph <autresvv@gmail.com>
2020-08-03 12:14:15 +0200

    Fix for Migration & Migration Work In Progress Build

--- build/CompleteBuild.CSHTML5.bat
+++ build/CompleteBuild.CSHTML5.bat
@@ -26,7 +26,7 @@ echo. 2>..\src\Runtime\Runtime\bin\CSHTML5\Migration\Debug.Simulator\System.dll
 echo.
 echo %ESC%[95mPacking %ESC%[0mCSHTML5.Migration %ESC%[95mNuGet package%ESC%[0m
 echo.
-nuget.exe pack data\CSHTML5.nuspec -OutputDirectory "output/CSHTML5" -Properties "PackageId=CSHTML5.Migration;PackageVersion=2.0.0-alpha%PackageVersion%;Configuration=Migration;AssembliesPrefix=CSHTML5.Migration;CompilerPrefix=SLMigration.CSharpXamlForHtml5"
+nuget.exe pack data\CSHTML5.nuspec -OutputDirectory "output/CSHTML5" -Properties "PackageId=CSHTML5.Migration;Target=CSHTML5.Migration;PackageVersion=2.0.0-alpha%PackageVersion%;Configuration=Migration;AssembliesPrefix=CSHTML5.Migration;CompilerPrefix=SLMigration.CSharpXamlForHtml5"
 
 echo.
 echo %ESC%[95mBuilding %ESC%[0mMigration.WorkInProgress %ESC%[95mconfiguration%ESC%[0m
@@ -44,7 +44,7 @@ echo. 2>..\src\Runtime\Runtime\bin\CSHTML5\Migration.WorkInProgress\Debug.Simula
 echo.
 echo %ESC%[95mPacking %ESC%[0mCSHTML5.Migration.WorkInProgress %ESC%[95mNuGet package%ESC%[0m
 echo.
-nuget.exe pack data\CSHTML5.nuspec -OutputDirectory "output/CSHTML5" -Properties "PackageId=CSHTML5.Migration.WorkInProgress;PackageVersion=2.0.0-alpha%PackageVersion%;Configuration=Migration.WorkInProgress;AssembliesPrefix=CSHTML5.Migration;CompilerPrefix=SLMigration.CSharpXamlForHtml5"
+nuget.exe pack data\CSHTML5.nuspec -OutputDirectory "output/CSHTML5" -Properties "PackageId=CSHTML5.Migration.WorkInProgress;Target=CSHTML5.Migration";PackageVersion=2.0.0-alpha%PackageVersion%;Configuration=Migration.WorkInProgress;AssembliesPrefix=CSHTML5.Migration;CompilerPrefix=SLMigration.CSharpXamlForHtml5"
 
 echo.
 echo %ESC%[95mBuilding %ESC%[0mDebug %ESC%[95mconfiguration%ESC%[0m
@@ -57,7 +57,7 @@ msbuild ../src/CSHTML5.sln -p:Configuration=Debug.Simulator
 echo.
 echo %ESC%[95mPacking %ESC%[0mCSHTML5 %ESC%[95mNuGet package%ESC%[0m
 echo.
-nuget.exe pack data\CSHTML5.nuspec -OutputDirectory "output/CSHTML5" -Properties "PackageId=CSHTML5;PackageVersion=2.0.0-alpha%PackageVersion%;Configuration=Debug;AssembliesPrefix=CSHTML5;CompilerPrefix=CSharpXamlForHtml5"
+nuget.exe pack data\CSHTML5.nuspec -OutputDirectory "output/CSHTML5" -Properties "PackageId=CSHTML5;;Target=CSHTML5;PackageVersion=2.0.0-alpha%PackageVersion%;Configuration=Debug;AssembliesPrefix=CSHTML5;CompilerPrefix=CSharpXamlForHtml5"
 
 
 echo.
--- restore-packages-cshtml5.bat
+++ restore-packages-cshtml5.bat
@@ -1,4 +1,4 @@
 build\nuget.exe install CSHTML5 -Version 2.0.0-alpha70-091 -OutputDirectory src\packages
-build\nuget.exe install CSHTML5.Migration -Version 2.0.0-alpha69-089 -OutputDirectory src\packages
-build\nuget.exe install CSHTML5.Migration.WorkInProgress -Version 2.0.0-alpha69-089 -OutputDirectory src\packages
+build\nuget.exe install CSHTML5.Migration -Version 2.0.0-alpha70-091 -OutputDirectory src\packages
+build\nuget.exe install CSHTML5.Migration.WorkInProgress -Version 2.0.0-alpha70-091 -OutputDirectory src\packages
 pause
